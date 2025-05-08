6c4960a1db5dfc86a55aab7ad118bb6d6e347caa
Patrick Crain <patrickacrain@gmail.com>
2025-05-08 16:14:48 -0400

    Add pooling for RaidenBeamController and ReverseBeamController as well

---
+++ src/BeamPoolers/RaidenBeamPooler.cs
@@ -0,0 +1,105 @@
+namespace GGV;
+
+using static RaidenBeamController;
+
+[HarmonyPatch]
+internal static class RaidenBeamPooler
+{
+    private static readonly LinkedList<Bone> _BonePool = new();
+    private static int _TotalBones = 0;
+    private static bool _Enabled = false; // cannot be enabled / disabled at run time without causing issues, so lock it in during patching
+
+    [HarmonyPatch(typeof(Bone), MethodType.Constructor, new[] {typeof(Vector2)})]
+    [HarmonyPostfix]
+    private static void NewBoneCounter()
+    {
+      GGVDebug.Log($"constructed {++_TotalBones} bones");
+    }
+
+    /// <summary>Replaces all calls to constructor</summary>
+    private static Bone Rent(Vector2 pos)
+    {
+        if (_BonePool.Count == 0)
+          _BonePool.AddLast(new Bone(default));
+
+        LinkedListNode<Bone> node = _BonePool.Last;
+        _BonePool.RemoveLast();
+
+        Bone bone                  = node.Value;
+        bone.pos                   = pos;
+        bone.normal                = default;
+
+        return bone;
+    }
+
+    /// <summary>Replaces all calls to Clear() and must be called in OnDestroy()</summary>
+    private static void ReturnAll(LinkedList<Bone> bones)
+    {
+        if (bones == null)
+          return;
+        while (bones.Count > 0)
+        {
+          LinkedListNode<Bone> node = bones.Last;
+          bones.RemoveLast();
+          _BonePool.AddLast(node);
+        }
+        // GGVDebug.Log($"returned {_BonePool.Count} / {_TotalBones} bones");
+    }
+
+    private static bool ReplaceConstructor(this ILCursor cursor)
+    {
+        if (!cursor.TryGotoNext(MoveType.Before, instr => instr.MatchNewobj<Bone>()))
+          return false;
+        // GGVDebug.Log($"replacing bone construction with rental (int)");
+        cursor.Remove();
+        cursor.CallPrivate(typeof(RaidenBeamPooler), nameof(RaidenBeamPooler.Rent));
+        return true;
+    }
+
+    private static bool ReplaceClear(this ILCursor cursor)
+    {
+        if (!cursor.TryGotoNext(MoveType.Before, instr => instr.MatchCallvirt(typeof(LinkedList<>).MakeGenericType(typeof(Bone)).GetMethod("Clear"))))
+          return false;
+        // GGVDebug.Log($"replacing Clear() with ReturnAll()");
+        cursor.Remove();
+        cursor.CallPrivate(typeof(RaidenBeamPooler), nameof(RaidenBeamPooler.ReturnAll));
+        return true;
+    }
+
+    /// <summary>Replace all construction of Bones with Rents</summary>
+    [HarmonyPatch(typeof(RaidenBeamController), nameof(RaidenBeamController.DrawBezierCurve))]
+    [HarmonyILManipulator]
+    private static void StartPatch(ILContext il)
+    {
+        _Enabled = GGVConfig.OPT_BEAMS;
+        if (!_Enabled)
+          return;
+        ILCursor cursor = new ILCursor(il);
+        if (!cursor.ReplaceConstructor()) return;
+        if (!cursor.ReplaceConstructor()) return;
+        GGVDebug.Log("patched RaidenBeamController.DrawBezierCurve!");
+    }
+
+    /// <summary>Replace all construction of Bones with Rents</summary>
+    [HarmonyPatch(typeof(RaidenBeamController), nameof(RaidenBeamController.HandleBeamFrame))]
+    [HarmonyILManipulator]
+    private static void HandleBeamFramePatch(ILContext il)
+    {
+        _Enabled = GGVConfig.OPT_BEAMS;
+        if (!_Enabled)
+          return;
+        ILCursor cursor = new ILCursor(il);
+        if (!cursor.ReplaceClear()) return;
+        GGVDebug.Log("patched RaidenBeamController.HandleBeamFrame!");
+    }
+
+    [HarmonyPatch(typeof(RaidenBeamController), nameof(RaidenBeamController.OnDestroy))]
+    [HarmonyPrefix]
+    static void BasicBeamControllerOnDestroyPatch(RaidenBeamController __instance)
+    {
+        if (!_Enabled)
+            return;
+        if (__instance.m_bones != null)
+            ReturnAll(__instance.m_bones);
+    }
+}
